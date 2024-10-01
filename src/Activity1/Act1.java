11d791062354afd22792c010723e45c7507dfbdd
oshimasen <jake.batucan@awsys-i.com>
2024-10-01 13:48:37 +0800

    Activity 1 branch

---
+++ src/Activity1/Act1.java
@@ -0,0 +1,18 @@
+package Activity1;
+
+public class Act1 {
+	public static void main(String[] args) {
+		String myName = "Jake Batucan";
+		String favoriteAnime = "One Piece";
+		String favoriteSport = "Volleyball";
+		String company = "AWS";
+		String hobby = "manga";
+
+		System.out.println("Hi, my name is " + myName + ". I like watching anime and my most favorite anime is "
+				+ favoriteAnime + ".\nI also play a lot of sports and my favorite one is " + favoriteSport
+				+ ".\nAside from that I am also working in " + company
+				+ " where studying becomes my new hobby :)\nAnother thing is that I enjoy reading " + hobby
+				+ " whenever I feel tired from work or in life in general. \nThat's all, thank you for reading!");
+
+	}
+}
