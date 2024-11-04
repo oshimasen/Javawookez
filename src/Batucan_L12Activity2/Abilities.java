5a11d76185c730f3aabcae9b56e17da63b968519
oshimasen <jake.batucan@awsys-i.com>
2024-11-04 16:22:12 +0800

    Lesson 12 Activity 2

---
+++ src/Batucan_L12Activity2/Abilities.java
@@ -0,0 +1,11 @@
+package Batucan_L12Activity2;
+
+/*
+ * Interface class in which each of the characters will be inheriting these functions from the abstract class,
+ */
+public interface Abilities {
+
+	public void restoreHealth(int restore);
+
+	public void buffAbility(int buff);
+}
---
+++ src/Batucan_L12Activity2/Archer.java
@@ -0,0 +1,35 @@
+package Batucan_L12Activity2;
+
+public class Archer extends DnDCharacter {
+
+	/*
+	 * Initializes the fields
+	 * 
+	 * @param characterName holds String value
+	 */ 
+	Archer(String characterName) {
+		this.characterName = characterName;
+		this.health = 100;
+		this.damage = 20;
+	}
+	/*
+	 * Displays character name and buff damage and attack
+	 * Calls receiveDamage method of opponent
+	 * 
+	 * @param opponent holds DnDCharacter value
+	 * @return no return value
+	 */
+	public void attack(DnDCharacter opponent) {
+		System.out.println(characterName + " buff damage: " + damage);
+		System.out.println(characterName + " sharply shoots " + opponent.characterName);
+		opponent.receiveDamage(damage);
+	}
+	/*
+	 * Calculate/update the health of the character depending on the given damage.
+	 * @param damage holds int value
+	 * @return no return value
+	 */
+	public void receiveDamage(int damage) {
+		health -= damage;
+	}
+}
